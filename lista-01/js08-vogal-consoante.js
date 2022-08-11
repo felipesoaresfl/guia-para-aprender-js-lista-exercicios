ad087b6b3bc132463bb737ea4686b1e2bd90d6a1
Felipe Soares <felipe.soares013@outlook.com>
2022-08-11 15:54:38 -0300

    feito js08

--- lista-01/js08-vogal-consoante.js
+++ lista-01/js08-vogal-consoante.js
@@ -1,18 +1,19 @@
 /**
- * Faça um Programa que verifique se uma letra 
+ * Faça um Programa que verifique se uma letra
  * de entrada é vogal ou consoante.
- * 
+ *
  * Entrada           Saída
  * a                 vogal
  * I                 vogal
  * z                 consoante
  * J                 consoante
-*/
+ */
 
 // Entrada
-var entrada = 'a'
+
+var entrada = "a";
+entrada = entrada.toLowerCase();
+var vogais = ["a", "e", "i", "o", "u"];
 
 // implemente aqui o condicional para retornar o boolean (true/false) com base na entrada
-var resultado = 'vogal'
-
-console.log(resultado)
+console.log(vogais.includes(entrada) ? "vogal" : "consoante");
