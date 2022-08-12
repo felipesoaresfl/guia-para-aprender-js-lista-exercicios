62cf965e782c8e8ed3a13508c6ccd9f33bd1a73d
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 12:36:05 -0300

    feito js02 lista 02

--- lista-02/js02-soma-lista.js
+++ lista-02/js02-soma-lista.js
@@ -1,16 +1,19 @@
 /**
  * Faça um programa que faça a soma de todos os números de uma lista de números
- * 
+ *
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10]       34
- * [1,1,40]             40
+ * [1,1,40]             42
  */
 
 // Entrada
-var entrada = [1,3,5,7,8,10]
+var entrada = [1, 3, 5, 7, 8, 10];
 
-var soma = 0
+var soma = 0;
 
 // percorrer a lista e fazer a somatória dos elementos
-
-console.log(soma)
+for (var i of entrada) {
+  soma += i;
+}
+// gerar a lista de saida
+console.log("👉 ", soma);
