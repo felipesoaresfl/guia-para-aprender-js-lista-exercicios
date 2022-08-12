32bcac5588fa0346404e6388ee09e05d4d27aa65
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 12:56:24 -0300

    feito js05 lista 02

--- lista-02/js05-max-2.js
+++ lista-02/js05-max-2.js
@@ -1,20 +1,35 @@
 /**
- * Faça um programa que mostre o segundo maior número da lista 
- * 
+ * Faça um programa que mostre o segundo maior número da lista
+ *
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10,3]      8
  * [1,1,40]              1
  * [40,42,1,0,-10]      40
- * 
+ *
  * IMPORTANTE:
  * Não usar sort
  * Não usar funções built-ins da linguagem
  */
 
 // Entrada
-var entrada = [40,42,1,0,-10]
+var entrada = [40, 42, 1, 0, -10];
 
-var segundoMaior = 40
+var segundoMaior = 40;
 
+var maior = 0;
+for (var i in entrada) {
+  if (entrada[i] > maior) {
+    maior = entrada[i];
+    var j = i;
+  }
+}
+entrada.splice(j, 1);
 
-console.log(segundoMaior)
+var segundoMaior = 0;
+for (var i of entrada) {
+  if (i > segundoMaior) {
+    segundoMaior = i;
+  }
+}
+
+console.log("👉 " + segundoMaior);
