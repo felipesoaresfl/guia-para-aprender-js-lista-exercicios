7bc4cb31a5bd12e8f98fad019f4ca49de282c1f8
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 12:49:45 -0300

    feito js04 lista 02

--- lista-02/js04-max.js
+++ lista-02/js04-max.js
@@ -1,6 +1,6 @@
 /**
  * Faça um programa que faça a soma de todos os números de uma lista de números
- * 
+ *
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10,3]     10
  * [1,1,40]             40
@@ -8,9 +8,14 @@
  */
 
 // Entrada
-var entrada = [1,1,40]
+var entrada = [1, 1, 40];
 
-var maior = 0
+var maior = 0;
 
+for (var i of entrada) {
+  if (i > maior) {
+    maior = i;
+  }
+}
 
-console.log(maior)
+console.log("👉 " + maior);
