d39ffe0796f1bc8430d504470012caddaef20ebd
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 13:22:48 -0300

    feito js07 lista 02

--- lista-02/js07-tabuada.js
+++ lista-02/js07-tabuada.js
@@ -1,23 +1,24 @@
 /**
- * Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. 
- * O usuário deve informar de qual numero ele deseja ver a tabuada. 
- * A saída deve ser conforme o exemplo abaixo: 
- * 
+ * Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+ * O usuário deve informar de qual numero ele deseja ver a tabuada.
+ * A saída deve ser conforme o exemplo abaixo:
+ *
  * ENTRADA: 5
- * 
+ *
  * SAIDA
  * 5 X 1 = 5
  * 5 X 2 = 10
  *  ...
- * 5 X 10 = 50 
- * 
+ * 5 X 10 = 50
+ *
  * Se a entrada for fora de 1 e 10, podemos imprimir: entrada inválida
  * gere um array com os valores da tabuada, separando assim os dados da exibição
  */
 
 // Entrada
-var entrada = 5
+var entrada = 15;
 
-var tabuada = [5, 10, 15]
-
-console.log(tabuada)
+for (i = 1; i < 11; i++) {
+  var result = entrada * i;
+  console.log("👉 " + entrada + " X " + i + " = " + result);
+}
