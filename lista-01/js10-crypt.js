7d88964e3a45c74b7756e6ff70b0fd8a6c187282
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 10:58:43 -0300

    feito js10

--- lista-01/js10-crypt.js
+++ lista-01/js10-crypt.js
@@ -1,13 +1,13 @@
 /**
  * Faça um programa que dado um texto de entrada, imprima o texto
  * criptografado. A criptografia inicial é bem simples,
- * apenas converta a para b, b para c e assim por diante. 
- * 
+ * apenas converta a para b, b para c e assim por diante.
+ *
  * ENTRADA          SAÍDA
  * abacate          bcbdbuf
  * zebra            @fcsb
  * Zebra            $fcsb
- * 
+ *
  * DICAS:
  * É uma variante do exercício js06
  * Pesquise a tabela ASCII, ai vamos ver que a = 97, que A = 65
@@ -15,10 +15,10 @@
  * Pesquise os métodos de String fromCharCode que converte a para 97
  * e charCodeAt que faz 97 voltar para a
  * Assim, no podemos usar um método bem simples de criptografia que seria mudar
- * o código ASCII de forma a incrementar 1, ou seja, mudar de a para b, b para c 
+ * o código ASCII de forma a incrementar 1, ou seja, mudar de a para b, b para c
  * e assim por diante!
  * quando for z ou Z mudar para @ e $ respectivamente ao invés de {
- * 
+ *
  * Tarefa
  * - entenda como converter a para 97
  * - percorra o abacate para gerar os ordinais correspondente: 97,  98, 97 ...
@@ -30,10 +30,22 @@
  */
 
 // Entrada
-var texto = 'abacate'
+var texto = "Zebra";
+var textoCripto = [];
 
 // implemente aqui uma lógica para criptografar o texto
 
-var textoCripto = 'bcbdbuf'
+for (var cont = 0; cont < texto.length; cont++) {
+  textoCripto.push(texto.charCodeAt(cont));
+  if (textoCripto[cont] == 122) {
+    textoCripto[cont] = 64;
+  } else if (textoCripto[cont] == 90) {
+    textoCripto[cont] = 36;
+  } else {
+    textoCripto[cont] += 1;
+  }
+  textoCripto[cont] = String.fromCharCode(textoCripto[cont]);
+}
+textoCripto = textoCripto.join("");
 
-console.log(textoCripto)
+console.log(textoCripto);
