53c4767c0654a8e24156319150000404b0260527
Felipe Soares <felipe.soares013@outlook.com>
2022-08-12 13:20:19 -0300

    feito js06 lista 02

--- lista-02/js06-range-impar.js
+++ lista-02/js06-range-impar.js
@@ -1,15 +1,21 @@
 /**
- * Faça um programa que gere apenas os números ímpares entre 1 e 150. 
- * 
+ * Faça um programa que gere apenas os números ímpares entre 1 e 150.
+ *
  * ENTRADA              SAÍDA
  * [1,150]              [1,3,5,7,9,11...147,149]
- * 
+ *
  */
 
 // Entrada
-var range = [1, 150]
+var range = [1, 150];
 
-var impares = []
+var impares = [];
 
+for (var i = range[0]; i < range[1]; i++) {
+  console.log(i);
+  if (i % 2 > 0) {
+    impares.push(i);
+  }
+}
 
-console.log(impares)
+console.log("👉 " + impares);
